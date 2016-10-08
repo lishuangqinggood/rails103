44b549df7e1b7e4f396d0d6823d008b1d45fe488
Lishuangqing <shuangqing@TIANYUANdeMacBook-Pro.local>
2016-10-08 18:54:53 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,18 +5,23 @@ class GroupsController < ApplicationController
   def new
     @group = Group.new
   end
-  def create
-    @group = Group.new(group_params)
-    @group.save
-
-      redirect_to groups_path
-  end
+  
   def edit
     @group = Group.find(params[:id])
   end
   def show
     @group = Group.find(params[:id])
   end
+  def create
+    @group = Group.new(group_params)
+
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
   def update
     @group = Group.find(params[:id])
 
